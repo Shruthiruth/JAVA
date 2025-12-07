34e2c75b80bc085ddd8f4f4826c6b966df44712d
Shruthiruth <shruthiruth21@gmail.com>
2025-12-07 19:14:22 +0530

    program to chunk string split

---
+++ Assignment-own problem/test6.java
@@ -0,0 +1,26 @@
+//“Write a Java program to split a given string into chunks of size n.
+//If the last chunk has fewer characters, include it as it is.
+//Do not use built-in substring split methods.
+
+
+class test6 {
+    public static void main(String[] args) {
+        String s = "abcdefghij";
+        int n = 3;
+        String chunk = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            chunk = chunk + s.charAt(i);  // append character to chunk
+
+            if ((i + 1) % n == 0) {
+                System.out.println(chunk); // print chunk in new line
+                chunk = "";               // reset chunk
+            }
+        }
+
+        // print last chunk if it exists
+        if (!chunk.equals("")) {
+            System.out.println(chunk);
+        }
+    }
+}
