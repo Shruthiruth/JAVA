393026283f595520ad9c4188303420ce4e5b3d18
shruthika <shruthiruth21@gmail.com>
2025-12-15 21:28:30 +0530

    binary search

---
+++ Assignment-own problem/test10.java
@@ -0,0 +1,36 @@
+// “Given a sorted integer array and a target value, write a Java program to find the index of the target using binary search.
+// If the target is not present, return -1.”
+
+public class test10
+{
+    public static void main(String[] args) {
+        int arr[]={1, 3, 5, 7, 9, 11};
+        int target=7;
+
+        int low=0;
+        int high=arr.length-1;
+
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+
+            if(arr[mid]==target)
+            {
+                System.out.println(mid);
+                return;
+            }
+            else if(arr[mid]<target)
+            {
+                low=mid+1;
+            }
+            else if(arr[mid]>target)
+            {
+                high=mid-1;
+            }
+        }
+
+        System.out.println(-1);
+        
+        
+    }
+}
