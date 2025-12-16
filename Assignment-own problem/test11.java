5847746af185b80e39eb792c78abed449c73afb4
shruthika <shruthiruth21@gmail.com>
2025-12-16 11:35:41 +0530

    bubble sort

---
+++ Assignment-own problem/test11.java
@@ -0,0 +1,29 @@
+public class test11
+{
+    public static void main(String[] args) {
+        int arr[]={5, 1, 4, 2, 8};
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<arr.length-1;j++)
+            {    
+
+                if(arr[j]>arr[j+1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;                    
+                }
+            }
+        }
+        System.out.print("[");
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]);
+            if(i!=arr.length-1)
+            {
+                System.out.print(",");
+            }
+        }
+        System.out.print("]");
+    }
+}
