66a174819619fc9c7f958dfcad62a6462ae7c09b
shruthika <shruthiruth21@gmail.com>
2025-12-18 11:30:28 +0530

    exeception error

--- DAY 23/test5.java
+++ DAY 23/test5.java
@@ -1,7 +1,9 @@
 class test5
 {
     public static void main(String[] args) {
-        Thread.sleep(500); //interrupted exeception
+        //Thread.sleep(500); //interrupted exeception
+        Thread t=new Thread();
+        t.setPriority(100); //illegal argument exeception
         System.out.println("GM");
     }
 }
