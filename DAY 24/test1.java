f4335d54c087f2c598c7a7ae8f5f3e1d2e93582d
shruthika <shruthiruth21@gmail.com>
2025-12-19 11:38:38 +0530

    exception handling using checked exception

---
+++ DAY 24/test1.java
@@ -0,0 +1,11 @@
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+
+class test1
+{
+    public static void main(String[] args) throws FileNotFoundException {
+        PrintWriter p1=new PrintWriter("abc1.txt");
+    }
+}
---
+++ DAY 24/test2.java
@@ -0,0 +1,13 @@
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+class test2
+{
+    public static void main(String[] args) {
+        try {
+            PrintWriter p=new PrintWriter("abc1.txt"); 
+        } catch (FileNotFoundException e) {
+        }
+    }
+}
