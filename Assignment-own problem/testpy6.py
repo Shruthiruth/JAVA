6e12056772822fd42ffd6b7f30717de1caf55196
shruthika <shruthiruth21@gmail.com>
2026-05-18 09:07:10 +0530

    anagram

---
+++ Assignment-own problem/testpy6.py
@@ -0,0 +1,9 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+def anagram(a,b):
+    if sorted(a)==sorted(b):
+        return True
+    else:
+        return False
+        
+print(anagram("hello","world"))
